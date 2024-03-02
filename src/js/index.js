0941d1e4c95d54597c999a2790fd5f3cef3a7692
Alexander <thorik.sanya@gmail.com>
2024-03-02 17:03:12 +1000

    Добавлено раскрытие списка машин в <= large

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import { swiper_catalog, swiper_nav } from "./modules/swiper";
-import "./modules/catalog";
+import { catalogToggle, carListToggle } from "./modules/catalog";
 import "./modules/accordion";
 import { selectorContainers } from "./modules/select";
 import "./modules/more";
 import "./modules/mobile-nav";
+
+catalogToggle();
+carListToggle();
 
 selectorContainers.forEach((selector) => {
   selector.addEventListener("change", (e) => {
--- src/js/modules/catalog.js
+++ src/js/modules/catalog.js
@@ -1,4 +1,4 @@
-export default (function catalogToggle() {
+export function catalogToggle() {
   const showButton = document.querySelector(".catalog__show-button");
   const collapse = document.querySelector(".catalog__collapse");
 
@@ -8,7 +8,21 @@ export default (function catalogToggle() {
     showButton.classList.toggle("catalog__show-button--collapsed");
     collapse.classList.toggle("catalog__collapse--show");
     const expandState = showButton.getAttribute("aria-expanded") === "true";
-    showButton.children[0].innerText = expandState ? "Развернуть список" : "Свернуть список машин";
+    showButton.children[0].innerText = expandState ? "Развернуть список машин" : "Свернуть список машин";
     showButton.setAttribute("aria-expanded", !expandState);
   });
-})();
+}
+
+export function carListToggle() {
+  const showButton = document.querySelector(".catalog__button-more");
+  const collapse = document.querySelector(".catalog__offers-car-articles");
+
+  if (!showButton || !collapse) return;
+
+  showButton.addEventListener("click", (e) => {
+    collapse.classList.toggle("catalog__offers-car-articles--show");
+    const expandState = showButton.getAttribute("aria-expanded") === "true";
+    showButton.innerText = expandState ? "Показать все машины" : "Свернуть";
+    showButton.setAttribute("aria-expanded", !expandState);
+  });
+}
